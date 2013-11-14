b105edebc925902d4f689cef47b163358c0e2870
Matt Lee <thatismatt@gmail.com>
2013-11-14 15:32:27 +0000

    Organise bindings. Add key bindings for dired mode.

--- config/bindings.el
+++ config/bindings.el
@@ -1,22 +1,36 @@
-;; Place your bindings here.
+;; text scaling
+;; (define-key global-map (kbd "C-+") 'text-scale-increase)
+;; (define-key global-map (kbd "C--") 'text-scale-decrease)
 
-;; For example:
-;;(define-key global-map (kbd "C-+") 'text-scale-increase)
-;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+;; kill buffer - without confirmation
+(define-key global-map (kbd "C-x k") 'kill-this-buffer)
 
-;; Move paredit sexp bracket slurp/barf commands from ctrl to alt key
+;; window & buffer switching
+(define-key global-map (kbd "S-<menu>") 'previous-buffer)
+(define-key global-map (kbd "<menu>")   'next-buffer)
+(define-key global-map (kbd "C-<menu>") 'win-switch-dispatch)
+
+;; undo tree
+(define-key global-map (kbd "C-, u") 'undo-tree-visualize)
+
+;; paredit - move sexp slurp/barf commands from ctrl to alt key
 (define-key paredit-mode-map (kbd "C-<right>") nil)
 (define-key paredit-mode-map (kbd "C-<left>") nil)
 (define-key paredit-mode-map (kbd "M-<right>") 'paredit-forward-slurp-sexp)
 (define-key paredit-mode-map (kbd "M-<left>") 'paredit-forward-barf-sexp)
 
-;; Window & Buffer switching with menu key
-(define-key global-map (kbd "S-<menu>") 'previous-buffer)
-(define-key global-map (kbd "<menu>")   'next-buffer)
-(define-key global-map (kbd "C-<menu>") 'win-switch-dispatch)
-
-;; Undo tree
-(define-key global-map (kbd "C-, u") 'undo-tree-visualize)
-
-;; HideShow mode - code folding
+;; hide show - code folding
 (add-hook 'prog-mode-hook #'hs-minor-mode)
+
+;; dired
+(define-key dired-mode-map (kbd "M-<up>") 'dired-prev-subdir)
+(define-key dired-mode-map (kbd "M-<down>") 'dired-next-subdir)
+(define-key dired-mode-map (kbd "M-<left>") 'dired-hide-all)
+(define-key dired-mode-map (kbd "M-<right>") 'dired-hide-subdir)
+(define-key dired-mode-map (kbd "C-k") 'dired-kill-subdir)
+(define-key dired-mode-map (kbd "M-k") 'dired-kill-tree)
+
+;; make q kill some buffers
+(define-key dired-mode-map (kbd "q") 'kill-this-buffer)
+(define-key help-mode-map (kbd "q") 'kill-this-buffer)
+(define-key magit-mode-map (kbd "q") 'kill-this-buffer)

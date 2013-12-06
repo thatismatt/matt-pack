a93f2287faba52398526d6966a5c4f77c794b8c4
Matt Lee <thatismatt@gmail.com>
2013-12-06 07:35:20 +0000

    Add d3 to js globals.

--- config/custom-configuration.el
+++ config/custom-configuration.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-global-externs (quote ("$" "angular" "_" "Highcharts" "google"))))
+ '(js2-global-externs (quote ("$" "angular" "_" "Highcharts" "google" "d3"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
